23429491cc289330a4398b61f14fa1a5632a2b82
tabbott117 <tyler3abbott@gmail.com>
2021-02-18 13:23:05 -0600

    Adds queue, stack, linkedlist function

--- python/linked_list.py
+++ python/linked_list.py
@@ -1,19 +1,50 @@
 class LinkList:
   # write your __init__ method here that should store a 'head' value which the first Node in the LinkedList and a 'length' value which is the total number of Nodes in the LinkedList
+  def __init__(self, head=None):
+    self.head = head
+    self.length = 0
 
   def add(self, data):
     # write your code to ADD an element to the Linked List
+    if self.head == None:
+      self.head = Node(data)
+      self.length += 1
+    else:
+      temp = self.head
+      while temp.next != None:
+        temp = temp.next
+      temp.next = Node(data)
+      self.length += 1
     pass
 
   def remove(self, data):
     # write your code to REMOVE an element from the Linked List
+    temp = self.head
+    if temp.data == data:
+      self.head = temp.next
+    else:
+      while temp.next != None:
+        if temp.next.data == data:
+          #Remove the required node from the list
+          temp.next = temp.next.next
+          break
+        else:
+          temp = temp.next
     pass
 
   def get(self, element_to_get):
     # write you code to GET and return an element from the Linked List
-    pass
+    counter = 0
+    temp = self.head
+    while counter != element_to_get:
+      temp = temp.next
+      counter += 1
+    return temp
 
 # ----- Node ------
 class Node:
   # store your DATA and NEXT values here
-  pass
+  def __init__(self, data):
+    self.data = data
+    self.next = None
+
--- python/queue.py
+++ python/queue.py
@@ -1,14 +1,24 @@
+# NOTE: I followed the instructions as given in the comments for this code. However, this is not the ideal way to program
+#  a Queue (dequeue should return the element dequeued and not the queue itself, etc.). I can redo it if need be!
+
 class Queue:
   # write your __init__ method here that should store a 'total' value which is the total number of elements in the Queue and a 'queue' value which is an array of stored values in the Queue
+  def __init__(self, queue=[]):
+    self.total = len(queue)
+    self.queue = queue
 
-  def enqueue(self):
+  def enqueue(self, data):
     # write your code to add data to the Queue following FIFO and return the Queue
-    pass
+    self.queue.insert(0, data)
+    self.total += 1
+    return self.queue
 
-  def dequeue(self, data):
+  def dequeue(self):
     # write your code to removes the data to the Queue following FIFO and return the Queue
-    pass
+    self.queue.pop()
+    self.total -= 1
+    return self.queue
 
   def size(self):
     # write your code that returns the size of the Queue
-    pass
+    return self.total
